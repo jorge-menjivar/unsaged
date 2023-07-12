9ac448cfd84f81c1e755fd207a5d85923035638e
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-07-11 18:46:56 -0700

    [Fix] crash when no default model var is set

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { ANTHROPIC_API_KEY, OPENAI_API_KEY } from '@/utils/app/const';
+import {
+  ANTHROPIC_API_KEY,
+  DEFAULT_MODEL,
+  OPENAI_API_KEY,
+} from '@/utils/app/const';
 import { dockerEnvVarFix } from '@/utils/app/docker/envFix';
 
 export { default } from '../components/Home/home';
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const defaultModelId = dockerEnvVarFix(process.env.DEFAULT_MODEL);
+  const defaultModelId = dockerEnvVarFix(DEFAULT_MODEL);
 
   return {
     props: {
