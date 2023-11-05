95a8baaddeb3c14e67b1f5506a7659414c51dbe9
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-11-05 12:08:06 -0800

    Fix token count

--- utils/server/ai_vendors/openai/getTokenCount.ts
+++ utils/server/ai_vendors/openai/getTokenCount.ts
@@ -1,94 +1,59 @@
 // Import necessary types and modules
 import { AiModel } from '@/types/ai-models';
 import { Message } from '@/types/chat';
+
+// @ts-ignore
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
+// @ts-ignore
 import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
+// @ts-ignore
 import wasm from '@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
-import { TokenCountTransaction } from '@/types/tokenCounts';
-import { storageCreateTokenCount } from '../../../app/storage/tokenCount';
-import { Database } from '@/types/database';
-import { User } from '@/types/auth';
 
-
-
-
-
-/// Initialization function
-async function initializeTiktoken() {
-  await init((imports) => WebAssembly.instantiate(wasm, imports));
-  return new Tiktoken(
-    tiktokenModel.bpe_ranks,
-    tiktokenModel.special_tokens,
-    tiktokenModel.pat_str,
-  );
-}
-
-// Function to calculate tokens for a text string
-function calculateTokens(encoding: any, text: string) {
-  return encoding.encode(text).length;
-}
-
-
-// Main token counting function
 export async function countTokensOpenAI(
   model: AiModel,
   systemPrompt: string,
   messages: Message[],
-  database: Database, // <-- Added this argument
-  user: User, // <-- Added this argument
-  countCompletion: boolean = false,
-  transactionType: string,
-  completionMessage?: string
 ) {
-  const encoding = await initializeTiktoken();
-  let tokens_per_message = model.id.includes('gpt-3.5') ? 5 : 4;
+  await init((imports) => WebAssembly.instantiate(wasm, imports));
+  const encoding = new Tiktoken(
+    tiktokenModel.bpe_ranks,
+    tiktokenModel.special_tokens,
+    tiktokenModel.pat_str,
+  );
 
-  let inputTokenCount = calculateTokens(encoding, systemPrompt);
-  let outputTokenCount = 0;
-  let completionTokenCount = 0;
+  const prompt_tokens = encoding.encode(systemPrompt);
 
-  if (countCompletion && completionMessage) {
-    completionTokenCount += calculateTokens(encoding, completionMessage) + tokens_per_message;
+  let tokens_per_message = 0;
+  if (model.id === 'gpt-3.5-turbo' || model.id === 'gpt-35-az') {
+    tokens_per_message = 5;
+  } else if (model.id === 'gpt-4' || model.id === 'gpt-4-32k') {
+    tokens_per_message = 4;
   }
 
-  messages.forEach((message) => {
-    const tokens = calculateTokens(encoding, message.content) + tokens_per_message;
-    if (message.role === 'user') {
-      inputTokenCount += tokens;
-    } else if (message.role === 'assistant') {
-      outputTokenCount += tokens;
+  let tokenCount = prompt_tokens.length + tokens_per_message;
+
+  for (let i = messages.length - 1; i >= 0; i--) {
+    const message = {
+      role: messages[i].role,
+      content: messages[i].content,
+    };
+
+    const tokens = encoding.encode(message.content);
+
+    if (tokens) {
+      tokenCount += tokens.length + tokens_per_message;
     }
-  });
-
-  if (inputTokenCount + outputTokenCount > model.requestLimit) {
-    encoding.free();
-    return { error: 'Token limit exceeded' };
+    if (tokenCount > model.requestLimit) {
+      encoding.free();
+      return { error: 'Token limit exceeded' };
+    }
   }
 
-  // Calculate the total token count
-  const totalTokenCount = inputTokenCount + outputTokenCount;
+  // every reply is primed with <|start|>assistant<|message|>
+  tokenCount += 3;
 
-  console.log('openai/getTokenCount.ts total token count', totalTokenCount);
-  console.log('openai/getTokenCount.ts model', model.id);
+  console.log('tokenCount', tokenCount);
 
-  // Create a new TokenCountTransaction object
-  const newTokenCountTransaction: TokenCountTransaction = {
-    totalTokenCount,  // this is calculated in your existing code
-    aimodel: model,  // 'model' is an existing parameter in your function
-    transactionType: "testValue"  // this is an existing parameter in your function
-    // ... populate other fields as necessary
-  };
-
-  console.log('getTokenCount.ts database', database);
-
-  // Insert the new transaction into the database
-  //storageCreateTokenCount(database, user, newTokenCountTransaction);
-
-  return {
-    inputCount: Math.ceil(inputTokenCount),
-    outputCount: Math.ceil(outputTokenCount),
-    completionCount: Math.ceil(completionTokenCount),
-  };
-
-
+  encoding.free();
+  return { count: tokenCount };
 }
