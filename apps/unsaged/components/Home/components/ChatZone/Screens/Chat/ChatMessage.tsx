ae3c2577e86807ce73942c0410ba4e5f019f511a
Sebastian Weise <sebiweise20@gmail.com>
2023-12-04 10:08:21 +0100

    Display image url in conversation

--- apps/unsaged/components/Home/components/ChatZone/Screens/Chat/ChatMessage.tsx
+++ apps/unsaged/components/Home/components/ChatZone/Screens/Chat/ChatMessage.tsx
@@ -129,7 +129,11 @@ export const ChatMessage: FC<Props> = memo(
     const copyOnClick = () => {
       if (!navigator.clipboard) return;
 
-      navigator.clipboard.writeText(message.content).then(() => {
+      const copyText = selectedConversation?.model.type === 'text'
+        ? message.content
+        : JSON.parse(message.content)[0].url;
+
+      navigator.clipboard.writeText(copyText).then(() => {
         setMessageCopied(true);
         setTimeout(() => {
           setMessageCopied(false);
@@ -151,11 +155,10 @@ export const ChatMessage: FC<Props> = memo(
     return (
       <div
         className={`sm:px-4 lg:px-8
-        ${
-          message.role === 'assistant'
+        ${message.role === 'assistant'
             ? 'bg-gray-200 text-gray-800 dark:bg-[#444654] dark:text-gray-100'
             : 'bg-theme-light text-gray-800 dark:bg-theme-dark dark:text-gray-100'
-        }`}
+          }`}
         style={{ overflowWrap: 'anywhere' }}
       >
         <div className="group relative m-auto flex p-4 text-base md:max-w-2xl md:py-6 lg:max-w-full lg:px-0 2xl:max-w-5xl 3xl:max-w-6xl 4xl:max-w-7xl 5xl:max-w-[1920px] justify-center">
@@ -232,97 +235,103 @@ export const ChatMessage: FC<Props> = memo(
             )}
             {message.role === 'assistant' && (
               <div className="flex flex-row">
-                <MemoizedReactMarkdown
-                  className="flex-1"
-                  remarkPlugins={[remarkGfm, remarkMath]}
-                  // @ts-ignore
-                  rehypePlugins={[rehypeRaw, rehypeKatex]}
-                  components={{
-                    code({ node, inline, className, children, ...props }) {
-                      if (children.length) {
-                        if (children[0] == '▍') {
-                          return (
-                            <span className="animate-pulse cursor-default mt-1">
-                              ▍
-                            </span>
+                {selectedConversation?.model.type === "text" ?
+                  <>
+                    <MemoizedReactMarkdown
+                      className="flex-1"
+                      remarkPlugins={[remarkGfm, remarkMath]}
+                      // @ts-ignore
+                      rehypePlugins={[rehypeRaw, rehypeKatex]}
+                      components={{
+                        code({ node, inline, className, children, ...props }) {
+                          if (children.length) {
+                            if (children[0] == '▍') {
+                              return (
+                                <span className="animate-pulse cursor-default mt-1">
+                                  ▍
+                                </span>
+                              );
+                            }
+
+                            children[0] = (children[0] as string).replace(
+                              '`▍`',
+                              '▍',
+                            );
+                          }
+
+                          const match = /language-(\w+)/.exec(className || '');
+
+                          return !inline ? (
+                            <CodeBlock
+                              key={Math.random()}
+                              language={(match && match[1]) || ''}
+                              value={String(children).replace(/\n$/, '')}
+                              {...props}
+                            />
+                          ) : (
+                            <code className={className} {...props}>
+                              {children}
+                            </code>
                           );
-                        }
-
-                        children[0] = (children[0] as string).replace(
-                          '`▍`',
-                          '▍',
-                        );
-                      }
-
-                      const match = /language-(\w+)/.exec(className || '');
-
-                      return !inline ? (
-                        <CodeBlock
-                          key={Math.random()}
-                          language={(match && match[1]) || ''}
-                          value={String(children).replace(/\n$/, '')}
-                          {...props}
-                        />
-                      ) : (
-                        <code className={className} {...props}>
-                          {children}
-                        </code>
-                      );
-                    },
-                    table({ children }) {
-                      return (
-                        <table className="border-collapse border border-black px-3 py-1 dark:border-white">
-                          {children}
-                        </table>
-                      );
-                    },
-                    th({ children }) {
-                      return (
-                        <th className="break-words border border-black bg-gray-500 px-3 py-1 text-white dark:border-white">
-                          {children}
-                        </th>
-                      );
-                    },
-                    td({ children }) {
-                      return (
-                        <td className="break-words border border-black px-3 py-1 dark:border-white">
-                          {children}
-                        </td>
-                      );
-                    },
-                    a({ children, ...props }) {
-                      return (
-                        <a {...props} target="_blank">
-                          {children}
-                        </a>
-                      );
-                    },
-                    img({ src, alt, width, height }) {
-                      if (!width && !height) {
-                        width = '1024px';
-                        height = '1024px';
-                      }
-                      return (
-                        // eslint-disable-next-line @next/next/no-img-element
-                        <img
-                          src={src!}
-                          alt={alt!}
-                          width={parseInt(width as string)}
-                          height={parseInt(height as string)}
-                          className="m-1"
-                        />
-                      );
-                    },
-                  }}
-                >
-                  {`${message.content}${
-                    messageIsStreaming &&
-                    messageIndex ==
-                      (selectedConversationMessages.length ?? 0) - 1
-                      ? '`▍`'
-                      : ''
-                  }`}
-                </MemoizedReactMarkdown>
+                        },
+                        table({ children }) {
+                          return (
+                            <table className="border-collapse border border-black px-3 py-1 dark:border-white">
+                              {children}
+                            </table>
+                          );
+                        },
+                        th({ children }) {
+                          return (
+                            <th className="break-words border border-black bg-gray-500 px-3 py-1 text-white dark:border-white">
+                              {children}
+                            </th>
+                          );
+                        },
+                        td({ children }) {
+                          return (
+                            <td className="break-words border border-black px-3 py-1 dark:border-white">
+                              {children}
+                            </td>
+                          );
+                        },
+                        a({ children, ...props }) {
+                          return (
+                            <a {...props} target="_blank">
+                              {children}
+                            </a>
+                          );
+                        },
+                        img({ src, alt, width, height }) {
+                          if (!width && !height) {
+                            width = '1024px';
+                            height = '1024px';
+                          }
+                          return (
+                            // eslint-disable-next-line @next/next/no-img-element
+                            <img
+                              src={src!}
+                              alt={alt!}
+                              width={parseInt(width as string)}
+                              height={parseInt(height as string)}
+                              className="m-1"
+                            />
+                          );
+                        },
+                      }}
+                    >
+                      {`${message.content}${messageIsStreaming &&
+                        messageIndex ==
+                        (selectedConversationMessages.length ?? 0) - 1
+                        ? '`▍`'
+                        : ''
+                        }`}
+                    </MemoizedReactMarkdown>
+                  </> :
+                  <>
+                    <img src={JSON.parse(message.content)[0].url} />
+                  </>
+                }
 
                 <div className="md:-mr-8 ml-1 md:ml-0 flex flex-col md:flex-row gap-4 md:gap-1 items-center md:items-start justify-end md:justify-start">
                   {messagedCopied ? (
@@ -343,7 +352,7 @@ export const ChatMessage: FC<Props> = memo(
             )}
           </div>
         </div>
-      </div>
+      </div >
     );
   },
 );
