fd01e320f22b5c52be9cd3fddf189c2cc1aa1bfb
Sebastian Weise <sebiweise20@gmail.com>
2023-12-15 12:42:14 +0100

    Added advanced settings accordion

--- apps/unsaged/components/Home/components/SecondaryMenu/components/Menu/components/Screens/ModelSettings/ModelSettings.tsx
+++ apps/unsaged/components/Home/components/SecondaryMenu/components/Menu/components/Screens/ModelSettings/ModelSettings.tsx
@@ -11,17 +11,25 @@ import { TemperatureSlider } from './components/temperature';
 import { TopKInput } from './components/top-k';
 import { TopPSlider } from './components/top-p';
 import HomeContext from '@/components/Home/home.context';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/common/ui/accordion"
+import { useTranslations } from 'next-intl';
+
 
 const possibleParameters = {
-  'system_prompt': { modelType: 'text', component: SystemPromptSelect },
-  'temperature': { modelType: 'text', component: TemperatureSlider },
-  'max_tokens': { modelType: 'text', component: MaxTokensSlider },
-  'top_p': { modelType: 'text', component: TopPSlider },
-  'top_k': { modelType: 'text', component: TopKInput },
-  'repeat_penalty': { modelType: 'text', component: RepeatPenaltySlider },
-  'presence_penalty': { modelType: 'text', component: PresencePenaltySlider },
-  'stop': { modelType: 'text', component: StopInput },
-  'seed': { modelType: 'text', component: SeedInput },
+  'system_prompt': { modelType: 'text', component: SystemPromptSelect, advanced: false },
+  'temperature': { modelType: 'text', component: TemperatureSlider, advanced: true },
+  'max_tokens': { modelType: 'text', component: MaxTokensSlider, advanced: true },
+  'top_p': { modelType: 'text', component: TopPSlider, advanced: true },
+  'top_k': { modelType: 'text', component: TopKInput, advanced: true },
+  'repeat_penalty': { modelType: 'text', component: RepeatPenaltySlider, advanced: true },
+  'presence_penalty': { modelType: 'text', component: PresencePenaltySlider, advanced: true },
+  'stop': { modelType: 'text', component: StopInput, advanced: true },
+  'seed': { modelType: 'text', component: SeedInput, advanced: true },
 }
 
 const openAiSupportedParameters = [
@@ -76,6 +84,7 @@ const ollamaSupportedParameters = [
 ];
 
 export const ModelSettings = () => {
+  const t = useTranslations();
   const {
     state: { selectedConversation },
   } = useContext(HomeContext);
@@ -107,12 +116,25 @@ export const ModelSettings = () => {
     <div className="pt-2 px-1 space-y-4">
       <ModelSelect />
 
-      {supportedParameters.map((parameter, index) => {
+      {supportedParameters.filter(p => !p.advanced).map((parameter, index) => {
         if (parameter.modelType === model?.type) {
           const ParameterComponent = parameter.component;
           return <ParameterComponent key={index} />;
         }
       })}
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>{t('advancedSettings')}</AccordionTrigger>
+          <AccordionContent>
+            {supportedParameters.filter(p => p.advanced).map((parameter, index) => {
+              if (parameter.modelType === model?.type) {
+                const ParameterComponent = parameter.component;
+                return <ParameterComponent key={index} />;
+              }
+            })}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
