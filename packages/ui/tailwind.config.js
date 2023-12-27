3a989707b6423ea06fbc6231bac24001c25f8938
Sebastian Weise <sebiweise20@gmail.com>
2023-12-28 00:45:01 +0100

    Added dropdown menu

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+        "./**/*.{js,ts,jsx,tsx}",
         './app/**/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
