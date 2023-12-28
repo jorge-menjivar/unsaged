2615e3b900134d986fb9d659921f0ec7c0f9d78e
Sebastian Weise <sebiweise20@gmail.com>
2023-12-28 10:38:25 +0100

    Tailwind style fix

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        // "../../packages/ui/**/*.{js,ts,jsx,tsx}",
-        // "./**/*.{js,ts,jsx,tsx}",
+        "../../packages/ui/**/*.{js,ts,jsx,tsx}",
         './app/**/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
